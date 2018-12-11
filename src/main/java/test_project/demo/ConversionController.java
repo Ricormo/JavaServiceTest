d16507390a21cd2227a6abf266b8b5c011b86a53
Ricormo <jacobsr1992@gmail.com>
2018-12-11 22:13:04 +0100

    Added a method to convert xml to json and fixed the conversion of the json to xml. Removed files from the target.

--- src/main/java/test_project/demo/ConversionController.java
+++ src/main/java/test_project/demo/ConversionController.java
@@ -1,17 +1,23 @@
 package test_project.demo;
 
+import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConversionController {
 
-    @PostMapping(path="/convert", produces = MediaType.APPLICATION_XML_VALUE)
-    public Object convert(@RequestBody String request) throws JSONException {
+    @PostMapping(path="/convert/toXml", produces = MediaType.APPLICATION_XML_VALUE)
+    public Object convertToXml(@RequestBody String request) throws JSONException {
         JSONObject json = new JSONObject(request);
-        return XML.toString(json);
+        return XML.toString(json, "root");
+    }
+
+    @PostMapping(path="/convert/toJson", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object convertToJson(@RequestBody String request) {
+        JSONObject jsonObject = XML.toJSONObject(request);
+        return jsonObject.toString();
     }
 }
